4c9e4e2d46663ed78928ba858318a74ede3e9413
Sean Cassidy <sc1951@york.ac.uk>
2018-02-12 12:03:07 +0000

    Added color wheel functionality

--- visual.py
+++ visual.py
@@ -1,8 +1,11 @@
 from Tkinter import *
 from time import *
 
+colors = {0:'#FF0000', 1:'#FF7F00', 2:'#FFFF00', 3:'#00FF00', 4:'#0000FF', 5:'#4B0082', 6:'#9400D3'}
+
 class light(object):
     def __init__(self, color, x1, x2):
+        self.notch = 0
         self.color = color
         self.channels = []
         self.stand = c.create_polygon([x1,225, x1+((x2-x1)/2), 150, x2, 225], width=2,fill='#000000')
@@ -14,22 +17,32 @@ class light(object):
     def getChannels(self):
         return self.channels
 
+    def getNotch(self):
+        return self.notch
+
+    def setNotch(self, notch):
+        self.notch = notch
+
 class backlight(object):
     def __init__(self, x1, x2):
         self.light = c.create_oval(x1, 97, x2, 203, fill='#000000')
 
 def pressed():                          #function
-        print 'buttons are cool'
-        light1.changeColor('#FFFFFF')
+        #print 'buttons are cool'
+        notch = light1.getNotch()
+        t.insert(INSERT, 'notch:' + str(notch) + ' button pressed...\n')
+        light1.changeColor(colors[notch])
+        light1.setNotch((notch+1)%6)
+        light2.changeColor(colors[(notch-1)%6])
+        #light2.setNotch((notch+2)%6)
+        light3.changeColor(colors[(notch-2)%6])
+        #light3.setNotch((notch+3)%6)
 
 root = Tk()                             #main window
 frame = Frame(root)
 c = Canvas(frame, width=600, height=325, bd=4, bg='#222222')
 frame.pack()
 c.pack()
-
-#Generate stands
-#stand = c.create_polygon([85,225,150,150,215,225], width=2,fill='#000000')
 
 #Generate lights
 back1 = backlight(90, 210)
@@ -38,9 +51,6 @@ back3 = backlight(390, 510)
 light1 = light('#AA0000', 100,200)
 light2 = light('#00AA00', 250, 350)
 light3 = light('#0000AA', 400, 500)
-#x1=90
-#x2=210
-#100, 100, 500, 500, 300, 300, 200, 200,
 
 t = Text(frame, height=5, width=75, bg='#444444', fg='#BBBBBB')
 t.pack()
