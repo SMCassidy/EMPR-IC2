d5ab7de614a8e4b2a39da55352a8d3277980d2b3
Sean Cassidy <sc1951@york.ac.uk>
2018-02-17 00:51:56 +0000

    Cleared up code, removed backlights, added lock.

--- visual.py
+++ visual.py
@@ -1,11 +1,10 @@
 from Tkinter import *
 from time import *
-from threading import Thread
+from threading import Thread, Lock
 import random
 import Queue
 
 colors = {0:'#FF0000', 1:'#FF7F00', 2:'#FFFF00', 3:'#00FF00', 4:'#0000FF', 5:'#4B0082', 6:'#9400D3'}
-
 
 class light(object):
     def __init__(self, color, x1, x2):
@@ -13,7 +12,7 @@ class light(object):
         self.color = color
         self.channels = []
         self.stand = c.create_polygon([x1,225, x1+((x2-x1)/2), 150, x2, 225], width=2,fill='#000000')
-        self.light = c.create_oval(x1, 100, x2, 200, fill=color)
+        self.light = c.create_oval(x1, 100, x2, 200, fill=color, outline='black', width=7)
 
     def change(self,color):
         c.itemconfig(self.light,fill=color)
@@ -27,10 +26,6 @@ class light(object):
     def setNotch(self, notch):
         self.notch = notch
 
-class backlight(object):
-    def __init__(self, x1, x2):
-        self.light = c.create_oval(x1, 97, x2, 203, fill='#000000')
-
 def pressed():                          #function
     #print 'buttons are cool'
     notch = light1.getNotch()
@@ -41,7 +36,7 @@ def pressed():                          #function
     light3.change(colors[(notch-2)%6])
 
 def changer():
-    sleep(1)
+    sleep(0.2)
     r = lambda: random.randint(0,255)
     c = lambda: '#%02X%02X%02X' % (r(),r(),r())
     while True:
@@ -49,7 +44,9 @@ def changer():
         light1.change(c())
         light2.change(c())
         light3.change(c())
-        sleep(0.5)
+        sleep(0.2)
+
+lock = Lock()
 
 root = Tk()                             #main window
 frame = Frame(root)
@@ -58,9 +55,6 @@ frame.pack()
 c.pack()
 
 #Generate lights
-back1 = backlight(90, 210)
-back2 = backlight(240, 360)
-back3 = backlight(390, 510)
 light1 = light('#AA0000', 100,200)
 light2 = light('#00AA00', 250, 350)
 light3 = light('#0000AA', 400, 500)
@@ -72,8 +66,8 @@ button = Button(frame, text = 'Press', command = pressed)
 button.pack(pady=10, padx = 20)
 #pressed()
 
-mainThread = Thread(target=changer)
-mainThread.setDaemon(True)
-mainThread.start()
+changer_thread = Thread(target=changer)
+changer_thread.setDaemon(True)
+changer_thread.start()
 
 root.mainloop()
