ecae264194d1a108dc063fcc9e4c02698e9bbacd
Sean Cassidy <sc1951@york.ac.uk>
2018-02-17 00:34:19 +0000

    Added multithreading + random color changer.

--- visual.py
+++ visual.py
@@ -1,7 +1,11 @@
 from Tkinter import *
 from time import *
+from threading import Thread
+import random
+import Queue
 
 colors = {0:'#FF0000', 1:'#FF7F00', 2:'#FFFF00', 3:'#00FF00', 4:'#0000FF', 5:'#4B0082', 6:'#9400D3'}
+
 
 class light(object):
     def __init__(self, color, x1, x2):
@@ -11,7 +15,7 @@ class light(object):
         self.stand = c.create_polygon([x1,225, x1+((x2-x1)/2), 150, x2, 225], width=2,fill='#000000')
         self.light = c.create_oval(x1, 100, x2, 200, fill=color)
 
-    def changeColor(self,color):
+    def change(self,color):
         c.itemconfig(self.light,fill=color)
 
     def getChannels(self):
@@ -28,15 +32,24 @@ class backlight(object):
         self.light = c.create_oval(x1, 97, x2, 203, fill='#000000')
 
 def pressed():                          #function
-        #print 'buttons are cool'
-        notch = light1.getNotch()
-        t.insert(INSERT, 'notch:' + str(notch) + ' button pressed...\n')
-        light1.changeColor(colors[notch])
-        light1.setNotch((notch+1)%6)
-        light2.changeColor(colors[(notch-1)%6])
-        #light2.setNotch((notch+2)%6)
-        light3.changeColor(colors[(notch-2)%6])
-        #light3.setNotch((notch+3)%6)
+    #print 'buttons are cool'
+    notch = light1.getNotch()
+    t.insert(INSERT, 'notch:' + str(notch) + ' button pressed...\n')
+    light1.change(colors[notch])
+    light1.setNotch((notch+1)%6)
+    light2.change(colors[(notch-1)%6])
+    light3.change(colors[(notch-2)%6])
+
+def changer():
+    sleep(1)
+    r = lambda: random.randint(0,255)
+    c = lambda: '#%02X%02X%02X' % (r(),r(),r())
+    while True:
+        #color = '#%02X%02X%02X' % (r(),r(),r())
+        light1.change(c())
+        light2.change(c())
+        light3.change(c())
+        sleep(0.5)
 
 root = Tk()                             #main window
 frame = Frame(root)
@@ -58,4 +71,9 @@ t.pack()
 button = Button(frame, text = 'Press', command = pressed)
 button.pack(pady=10, padx = 20)
 #pressed()
+
+mainThread = Thread(target=changer)
+mainThread.setDaemon(True)
+mainThread.start()
+
 root.mainloop()
